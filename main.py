41f34063d076d24708583eeabd1031a08536657b
sans-twin <sans.twin.git@gmail.com>
2026-04-19 15:03:30 +0500

    guess_word2

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ words = ["ПЕЛЬМЕНИ", "ДРЕЛЬ", "РУЛЕТКА", "КИРПИЧ"]
 guess_word = choice(words)
 
 guessed_words = []
+print("Добро пожаловать!")
 
 while (word := input().lower()) != guess_word.lower():
-    if word != guess_word:
+    if word != guess_word and word not in guessed_words:
         guessed_words.append(word)
+        print("Вы не отгадали слово!")
     elif word == guess_word:
-        print("Вы угадли слово!")
+        print("Вы угадали слово!")
